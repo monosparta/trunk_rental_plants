aa2631b867b662c12974238002262ed56d68d777
VanLinLin <aas12as12as12tw@gmail.com>
2022-08-04 11:45:57 +0800

    chore: :zap: clean up test file

--- test.py
+++
@@ -1,7 +0,0 @@
-
-soil_humi_percent = 100*(1 - 1555 / 4095)
-print(F"{soil_humi_percent:4.2f}")
-
-a = map(lambda x: 100*(1 - x / 4095), [x for x in range(4096)])
-
-print([print(F"{i:4.2f}") for i in a])
